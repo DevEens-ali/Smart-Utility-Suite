805c09a5261b8550aa2da25a93556136cb7f3e43
Anees Ali <eensali01@gmail.com>
2026-08-05 11:54:54 +0500

    Update bmi_calculator.py

--- routers/bmi_calculator.py
+++ routers/bmi_calculator.py
@@ -1,11 +1,23 @@
 from fastapi import APIRouter
+from schemas.bmi_calculator import BMICalculatorRequest
+from services.bmi_service import BMICalculatorService
 
 router = APIRouter(
-    prefix = "/bmi",
-    tags = ["BMI calculator"]
+    prefix="/bmi",
+    tags=["BMI Calculator"]
 )
+
+
 @router.get("/")
-def BMICalculator():
+def get_bmi_info():
     return {
-        "message": "BMI Calculator API"
+        "message": "Welcome! Check your BMI to track your health."
     }
+
+
+@router.post("/calculate")
+def calculatebmi(request: BMICalculatorRequest):
+    return BMICalculatorService.calculate(
+        request.height,
+        request.weight,
+    )
