93fbd2fcc8409f4be0334ad3bce3bb06630c1e5e
Anees Ali <eensali01@gmail.com>
2026-08-16 20:51:44 +0500

    Update scientific_calculator.py

---
+++ backend/routers/scientific_calculator.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field
+from typing import Annotated
+
+
+class ScientificCalculatorRequest(BaseModel):
+    """
+    Request model for the Scientific Calculator.
+    """
+
+    input_expression: Annotated[
+        str,
+        Field(..., description="Enter Mathematical Expression")
+    ]
