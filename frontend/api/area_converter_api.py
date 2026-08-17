b33823a8e4994166954ff0d686e0f6cd9be6d9c7
Anees Ali <eensali01@gmail.com>
2026-08-17 15:23:41 +0500

    Add files via upload

---
+++ frontend/api/area_converter_api.py
@@ -0,0 +1,30 @@
+import requests
+
+
+BASE_URL = "http://127.0.0.1:8000"
+
+
+def convert_area(value, from_unit, to_unit):
+
+    url = f"{BASE_URL}/area/convert"
+
+    payload = {
+        "value": value,
+        "from_unit": from_unit,
+        "to_unit": to_unit
+    }
+
+    try:
+        response = requests.post(
+            url,
+            json=payload,
+        )
+
+        response.raise_for_status()
+
+        return response.json()
+
+    except requests.RequestException as e:
+        return {
+            "error": str(e)
+        }
